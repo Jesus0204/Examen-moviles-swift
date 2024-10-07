7ad795b0e0a96b5e2281f01637acbb218f067845
Jesus0204 <a01705442@tec.mx>
2024-10-06 20:28:16 -0600

    feat: crear proyecto de XCode

---
+++ Examen Moviles/Examen_MovilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_MovilesApp.swift
+//  Examen Moviles
+//
+//  Created by Jesus Cedillo on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Examen_MovilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examen MovilesTests/Examen_MovilesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_MovilesTests.swift
+//  Examen MovilesTests
+//
+//  Created by Jesus Cedillo on 06/10/24.
+//
+
+import Testing
+@testable import Examen_Moviles
+
+struct Examen_MovilesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
